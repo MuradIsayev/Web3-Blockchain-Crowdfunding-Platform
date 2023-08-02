7c0c13abea29843bf3146af7b291a0ff85aea70d
MurAd <muradisayevazer@gmail.com>
2023-08-03 02:01:08 +0400

    needed components and pages imported in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { Navbar, Sidebar } from './components'
+import { CampaignDetails, Home, CreateCampaign, Profile } from './pages'
 
 const App = () => {
     return (
         <div className='relative sm:p-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
             <div className='sm:flex hidden mr-10 relative'>
-                Sidebar
+                <Sidebar />
             </div>
             <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
-                Navbar
+                <Navbar />
+
+                <Routes>
+                    <Route path='/' element={<Home />}></Route>
+                </Routes>
             </div>
         </div>
     )
