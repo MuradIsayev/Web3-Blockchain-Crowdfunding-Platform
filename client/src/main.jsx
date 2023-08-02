fa087f78adcbc4048f5f91c98fdf7e49e390cf49
MurAd <muradisayevazer@gmail.com>
2023-08-03 01:49:20 +0400

    index css imported

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { ThirdwebProvider } from '@thirdweb-dev/react'
 import { Sepolia } from "@thirdweb-dev/chains";
 import App from './App'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <ThirdwebProvider activeChain={Sepolia}>
+    <ThirdwebProvider activeChain={Sepolia} clientId="6a87676a3dcabc5fb677bc243ecf4f54">
         <Router>
             <App />
         </Router>
