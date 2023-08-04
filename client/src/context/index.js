f7bbe99d1795d33a7c5d026e52a03381f9abab6d
MurAd <muradisayevazer@gmail.com>
2023-08-05 01:30:51 +0400

    Context api initialized

---
+++ client/src/context/index.js
@@ -0,0 +1,8 @@
+import React, { useContext, createContext } from "react";
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react'
+import { ethers } from "ethers";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+}
